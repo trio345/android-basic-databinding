06b9835596ab118b647eacfa395d8a9aa99ce137
Trio <triosaputra308@gmail.com>
2020-08-10 18:58:06 +0700

    new updated

--- app/src/main/java/id/co/mdd/databindingexcercise/activities/MainActivity.java
+++ app/src/main/java/id/co/mdd/databindingexcercise/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package id.co.mdd.databindingexcercise.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import id.co.mdd.databindingexcercise.R;
@@ -13,12 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        showTerms();
     }
 
     public void FirstActivity(View view) {
 
     }
 
+    private void showTerms(){
+        View termsView;
+        LayoutInflater inflater;
+        inflater = getLayoutInflater();
+        termsView = inflater.inflate(R.layout.terms_condition, null);
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setView(termsView);
+    }
+
+
     public void SecondActivity(View view) {
     }
+
+
+
 }
