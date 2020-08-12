03f4aa1976d18a4f57d38d9f983edf68d0c16123
Trio <triosaputra308@gmail.com>
2020-08-12 10:42:34 +0700

    new update

--- app/src/main/java/id/co/mdd/databindingexcercise/activities/SecondActivity.java
+++ app/src/main/java/id/co/mdd/databindingexcercise/activities/SecondActivity.java
@@ -3,30 +3,30 @@ package id.co.mdd.databindingexcercise.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import id.co.mdd.databindingexcercise.R;
+import id.co.mdd.databindingexcercise.adapters.PostsAdapter;
 import id.co.mdd.databindingexcercise.databinding.ActivitySecondBinding;
 import id.co.mdd.databindingexcercise.viewmodels.PostViewModel;
 
 public class SecondActivity extends AppCompatActivity {
-    ActivitySecondBinding binding;
+    PostViewModel postViewModel;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
 
-        binding = DataBindingUtil.setContentView(SecondActivity.this, R.layout.post_layout);
-
-        PostViewModel postModel = new ViewModelProvider(this).get(PostViewModel.class);
-        postModel.fetchUsers();
-        postModel.getPosts().observe(this, postsModel -> {
-
-        });
+        postViewModel = new ViewModelProvider(this).get(PostViewModel.class);
+       postViewModel.fetchUsers();
+       postViewModel.
     }
 }
--- app/src/main/java/id/co/mdd/databindingexcercise/adapters/PostsAdapter.java
+++ app/src/main/java/id/co/mdd/databindingexcercise/adapters/PostsAdapter.java
@@ -1,42 +1,117 @@
 package id.co.mdd.databindingexcercise.adapters;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.databinding.DataBindingUtil;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
+import java.util.List;
 
-public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
-    private ArrayList<PostsAdapter> posts = new ArrayList<>();
+import id.co.mdd.databindingexcercise.R;
+import id.co.mdd.databindingexcercise.activities.UserDetailActivity;
+import id.co.mdd.databindingexcercise.databinding.ItemUserBinding;
+import id.co.mdd.databindingexcercise.databinding.PostLayoutBinding;
+import id.co.mdd.databindingexcercise.models.posts.PostModel;
+import id.co.mdd.databindingexcercise.models.user.DataItem;
+import id.co.mdd.databindingexcercise.models.user.UserModel;
+import retrofit2.Callback;
 
-    public void setPostsAdapter(ArrayList<PostsAdapter> posts){
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> implements Filterable {
+
+    private List<PostModel> posts = new ArrayList<>();
+    private static Context context;
+    private static List<PostModel> postsFiltered;
+
+
+    public void setUsersAdapter(Context context, List posts) {
+        this.context = context;
         this.posts = posts;
-
+        this.postsFiltered = posts;
         notifyDataSetChanged();
     }
 
-
-    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public PostsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        PostLayoutBinding binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.post_layout,
+                parent,
+                false
+        );
+        return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull PostsAdapter.ViewHolder holder, int position) {
+        holder.bind(postsFiltered.get(position));
     }
+
+
+    // filter
+    @Override
+    public Filter getFilter(){
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String str = charSequence.toString();
+                if (str.isEmpty()){
+                    postsFiltered = posts;
+                } else {
+                    List<PostModel> filtered = new ArrayList<>();
+                    for (PostModel data :posts) {
+                        if (data.getTitle().toLowerCase().contains(str.toLowerCase()) || data.getBody().toLowerCase().contains(str.toLowerCase())) {
+                            filtered.add(data);
+                        }
+                    }
+                    postsFiltered = filtered;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = postsFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                postsFiltered = (ArrayList<PostModel>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return postsFiltered.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        PostLayoutBinding binding;
+        public ViewHolder(@NonNull PostLayoutBinding binding){
+            super(binding.getRoot());
+
+            this.binding = binding;
+
+        }
+        public void bind(PostModel data){
+            binding.setPostModel(data);
         }
     }
 }
